3e020c47e258e15cf47330350cced3e08e43a690
gogicha007 <gogicha@gmail.com>
2025-02-16 22:33:13 +0400

    chore: save the progress

--- app/components/card/Card.tsx
+++ app/components/card/Card.tsx
@@ -1,1 +1,32 @@
 import './card.css';
+import type { ICharacterDetails } from '../../types/interface';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { toggleCardSelection } from '../../state/checkCards/selectedCardsSlice';
+// import type { RootState } from '../../state/store';
+
+export const Card = (data: ICharacterDetails) => {
+  //   const dispatch = useDispatch();
+  //   const selectedCards = useSelector(
+  //     (state: RootState) => state.selectedCards.selectedCards
+  //   );
+
+  const handleCheckboxClick = (event: React.MouseEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+    // dispatch(toggleCardSelection(data.id));
+  };
+
+  return (
+    <div className="card" role="article">
+      <img className="card__image" src={data.image} alt="" />
+      <p className="card__data">{data.name}</p>
+      <p className="card__data">{data.species}</p>
+      <p className="card__data">{data.status}</p>
+      <input
+        className="card__checkbox card__checkbox-enlarged"
+        type="checkbox"
+        // checked={selectedCards.includes(data.id)}
+        onClick={handleCheckboxClick}
+      />
+    </div>
+  );
+};
