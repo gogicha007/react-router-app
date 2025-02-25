77dd48563f75b3f27c85abc903b84b7ccb10a95c
gogicha007 <gogicha@gmail.com>
2025-02-25 09:44:49 +0400

    chore: save the progress

--- app/state/features/pickCards/PickCards.tsx
+++ app/state/features/pickCards/PickCards.tsx
@@ -1,18 +1,20 @@
 import type { RootState } from '~/state/store';
-import { useSelector, useDispatch } from 'react-redux';
-import Papa from 'papaparse';
-import { clearSelection } from './selectedCardsSlice';
 import type { IResponse } from '~/types/interface';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearSelection } from './selectedCardsSlice';
+import { useState } from 'react';
+import Papa from 'papaparse';
+import { FileDownloader } from '../../../components/file-downloader/FileDownloader';
 
 type Props = {
   data: IResponse;
 };
 const PickCards = (props: Props) => {
   const dispatch = useDispatch();
-
   const selectedCards = useSelector(
     (state: RootState) => state.selectedCards.selectedCards
   );
+  const [csvContent, setCsvContent] = useState<string | null>(null);
 
   const handleDownloadCSV = () => {
     if (!props.data || !props.data.results) return;
@@ -39,6 +41,7 @@ const PickCards = (props: Props) => {
         header: true,
       }
     );
+    setCsvContent(csv);
 
     const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
     const link = document.createElement('a');
@@ -53,6 +56,12 @@ const PickCards = (props: Props) => {
       <h2>Items selected: {selectedCards.length}</h2>
       <button onClick={() => dispatch(clearSelection())}>Deselect all</button>
       <button onClick={handleDownloadCSV}>Download CSV</button>
+      {csvContent && (
+        <FileDownloader
+          fileName={`${selectedCards.length}_characters.csv`}
+          fileContent={csvContent}
+        />
+      )}
     </>
   );
 };
